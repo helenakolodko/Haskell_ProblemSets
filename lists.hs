3cff482c20076d682c1b9c110204c858d261d4ee
Helena Kolodko <helena.kolodko@gmail.com>
2016-08-31 15:42:32 +0300

    added more list problems

--- lists.hs
+++ lists.hs
@@ -31,3 +31,11 @@ last' (_:xs) = last xs
 --18 Extract a slice from a list. 
 --19 Rotate a list N places to the left. 
 --20 Remove the K'th element from a list. 
+--21 Insert an element at a given position into a list. 
+--22 Create a list containing all integers within a given range. 
+--23 Extract a given number of randomly selected elements from a list. 
+--24 Draw N different random numbers from the set 1..M. 
+--25 Generate a random permutation of the elements of a list. 
+--26 Generate the combinations of K distinct objects chosen from the N elements of a list 
+--27 Group the elements of a set into disjoint subsets. 
+--28 Sorting a list of lists according to length of sublists.
